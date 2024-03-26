e5db290e90888b19eb9def1223219b967ba2f21e
zimmoc <128923890+zimmoc@users.noreply.github.com>
2024-03-26 20:32:37 +0100

    Add logic to dashboard views to render based on role

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from . import views
 # Create your views here.
@@ -6,7 +7,25 @@ from . import views
 @login_required
 def home(request):
 
-    return render(
-        request,
-        'dashboards/index.html'
-    )
+
+    if request.user.is_authenticated:
+        if request.user.role == 1:
+            return render(
+                request,
+                'dashboards/landlord.html'
+            )
+        elif request.user.role == 2:
+            return render(
+                request,
+                'dashboards/contractor.html'
+            )
+        elif request.user.role == 3:
+            return render(
+                request,
+                'dashboards/tenant.html'
+            )
+    else:
+        return render(
+            request,
+            'dashboards/none.html'
+        )
